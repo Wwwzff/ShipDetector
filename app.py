eb4ec9ce7d59ac4de26d58b42afa6dfa799e5438
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2018-11-14 12:32:59 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,40 +6,33 @@ import glob
 import re
 import numpy as np
 
-#PIL
+# PIL
 from PIL import Image 
 
+# Skimage
 from skimage.morphology import binary_opening, disk
 from skimage.io import imsave
 from skimage import img_as_uint
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
-from keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, make_response
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
+#global result path
 file1_path = ""
 
 # Define a flask app
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-model_path = '/Users/zifanwang/Documents/keras-flask-deploy-webapp/models/fullres_model.h5'
+model_path = './models/fullres_model.h5'
 
-# Load your trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
 model = load_model(model_path)
-print('Model loaded. Check http://127.0.0.1:5000/')
+print('Model loaded. Check http://localhost:5000/')
 
 def predictraw(image_path,model_path):
     model = load_model(model_path)
@@ -47,10 +40,8 @@ def predictraw(image_path,model_path):
     c_img = np.expand_dims(c_img, 0)/255.0
     cur_seg = model.predict(c_img)[0]
     return cur_seg
-
 def smooth(pred_raw):
     return binary_opening(pred_raw>0.99, np.expand_dims(disk(2), -1))
-
 def predict(image_path,model_path):
     predictimage = predictraw(image_path,model_path)
     return smooth(predictimage)
@@ -63,6 +54,7 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
+    global file1_path
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
@@ -75,20 +67,39 @@ def upload():
 
         preds = predict(file_path,model_path)
 
+
         file1_path = os.path.join(
             basepath, 'results', secure_filename(f.filename))
         
         # Process your result for human
         # pred_class = preds.argmax(axis=-1)            # Simple argmax
         imsave(file1_path,img_as_uint(preds[:,:,0]))
-        result = "Successed! "
-        return result
-
-    if request.method =="POST":
-        
+        #result = "Successed! "
+        #resultfilename =f.filename
+        return file1_path
     
+    if request.method =='GET':
+        if file1_path is None:
+            pass
+        else:
+            image_data = open(file1_path, "rb").read()
+            response = make_response(image_data)
+            response.headers['Content-Type'] = 'image/jpg'
+            return response
 
     return None
+
+############################################################################
+
+@app.route('/favicon.ico',methods =['GET'])
+def ico():
+    ico_path = './ship.ico'
+    ico_data = open(ico_path,'rb').read()
+    response = make_response(ico_data)
+    response.headers['Content-Type'] = 'image/ico'
+    return response
+
+
 
 
 if __name__ == '__main__':
