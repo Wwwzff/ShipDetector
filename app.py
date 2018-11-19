bee57a046d9c443257450f28b81a360535006fac
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2018-11-19 15:35:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def ico():
 if __name__ == '__main__':
     # app.run(port=5002, debug=True)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port)
     # Serve the app with gevent
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
+    http_server = WSGIServer(('0.0.0.0', port), app)
+    http_server.serve_forever()
